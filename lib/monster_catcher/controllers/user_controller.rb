bbc675ec7105ecc59dc241e4dfbe66a3433bd346
sleepingkingstudios <merlin@sleepingkingstudios.com>
2013-02-05 00:28:04 -0500

    Added UserController; added help actions to RoutingController
    - current spec status: 218 examples, 0 failures

--- lib/monster_catcher/controllers/user_controller.rb
+++ lib/monster_catcher/controllers/user_controller.rb
@@ -1,11 +1,12 @@
-# lib/monster_catcher/controllers/routing_controller.rb
+# lib/monster_catcher/controllers/user_controller.rb
 
-require 'mithril/controllers/proxy_controller'
+require 'mithril/controllers/abstract_controller'
 require 'mithril/controllers/mixins/help_actions'
+
 require 'monster_catcher/controllers'
 
 module MonsterCatcher::Controllers
-  class RoutingController < Mithril::Controllers::ProxyController
+  class UserController < Mithril::Controllers::AbstractController
     mixin Mithril::Controllers::Mixins::HelpActions
   end # class
 end # module
--- spec/monster_catcher/controllers/user_controller_spec.rb
+++ spec/monster_catcher/controllers/user_controller_spec.rb
@@ -1,16 +1,16 @@
-# spec/controllers/routing_controller_spec.rb
+# spec/monster_catcher/controllers/user_controller_spec.rb
 
 require 'monster_catcher/spec_helper'
-require 'mithril/controllers/proxy_controller_helper'
+require 'mithril/controllers/abstract_controller_helper'
 require 'mithril/controllers/mixins/help_actions_helper'
 
-require 'monster_catcher/controllers/routing_controller'
+require 'monster_catcher/controllers/user_controller'
 
-describe MonsterCatcher::Controllers::RoutingController do
+describe MonsterCatcher::Controllers::UserController do
   let :request do FactoryGirl.build :request; end
   let :described_class do Class.new super(); end
   let :instance do described_class.new request; end
   
-  it_behaves_like Mithril::Controllers::ProxyController
+  it_behaves_like Mithril::Controllers::AbstractController
   it_behaves_like Mithril::Controllers::Mixins::HelpActions
 end # describe
